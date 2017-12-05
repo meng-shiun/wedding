e02452efd11f42fae23ef7b13d061cd41cd99150
Meng-Shiun <lin.mengshiun@gmail.com>
2017-12-05 21:02:13 +0100

    First Commit

---
+++ js/music_player.js
@@ -0,0 +1,18 @@
+var ap = new APlayer({
+    element: document.getElementById('music-player'),                       // Optional, player element
+    narrow: false,                                                     // Optional, narrow style
+    autoplay: true,                                                    // Optional, autoplay song(s), not supported by mobile browsers
+    showlrc: 0,                                                        // Optional, show lrc, can be 0, 1, 2, see: ###With lrc
+    mutex: true,                                                       // Optional, pause other players when this player playing
+    theme: '#e6d0b2',                                                  // Optional, theme color, default: #b7daff
+    mode: 'random',                                                    // Optional, play mode, can be `random` `single` `circulation`(loop) `order`(no loop), default: `circulation`
+    preload: 'metadata',                                               // Optional, the way to load music, can be 'none' 'metadata' 'auto', default: 'auto'
+    listmaxheight: '513px',                                             // Optional, max height of play list
+    music: {                                                           // Required, music info, see: ###With playlist
+        title: 'Galway Bay',                                          // Required, music title
+        author: 'Celtic Woman',                          // Required, music author
+        url: 'GalwayBay.mp3',  // Required, music url
+        pic: 'images/album.jpg '  // Optional, music picture
+        // lrc: '[00:00.00]lrc here\n[00:01.00]aplayer'                   // Optional, lrc, see: ###With lrc
+    }
+});
